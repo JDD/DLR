d3a2646a103fcea13c561e844421167d1acfef18
JDD <Duncan@emailaccount.com>
2010-02-18 23:57:39 -0600

    forgot to remove unneeded collums when updateing for google voice

--- createdb.py
+++ createdb.py
@@ -82,7 +82,7 @@ Base.metadata.create_all()
 
 if round:
     print "Migrating users/friends"
-    session.execute(text("INSERT INTO users (id, name, alias, passwd, active, access, email, phone, pubphone, googlevoice, sponsor, quits, available_cookies, carebears, last_cookie_date) SELECT id, name, alias, passwd, active, access, email, phone, pubphone, googlevoice, sponsor, quits, available_cookies, carebears, last_cookie_date FROM %s.users;" % (round,)))
+    session.execute(text("INSERT INTO users (id, name, alias, passwd, active, access, email, phone, pubphone, googlevoice, sponsor) SELECT id, name, alias, passwd, active, access, email, phone, pubphone, googlevoice, sponsor, FROM %s.users;" % (round,)))
     session.execute(text("SELECT setval('users_id_seq',(SELECT max(id) FROM users));"))
     session.execute(text("INSERT INTO phonefriends (user_id, friend_id) SELECT user_id, friend_id FROM %s.phonefriends;" % (round,)))
     print "Migrating slogans/quotes"
