3c317270993f7b25125bdbd1637baa90e4cb0e39
JDD <Duncan@emailaccount.com>
2010-02-27 19:49:18 -0600

    bugfix/ells drunken rewrite minus the @route it was buged

--- Hooks/calcs/xp.py
+++ Hooks/calcs/xp.py
@@ -1,5 +1,5 @@
 # This file is part of Merlin.
-# Merlin is the Copyright (C)2008, 2009, 2010 of Robin K. Hansen, Elliot Rosemarine, Andreas Jacobsen.
+# Merlin is the Copyright (C)2008,2009,2010 of Robin K. Hansen, Elliot Rosemarine, Andreas Jacobsen.
 
 # Individual portions may be copyright by individual contributors, and
 # are included in this collective work with permission of the copyright
@@ -20,31 +20,35 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
 from Core.maps import Planet
-from Core.loadable import loadable, route
+from Core.loadable import loadable, route, require_planet
 
 class xp(loadable):
     usage = " <x:y:z> [a:b:c]"
 
     @route(r"%s(?:\s+%s)?"%((loadable.planet_coord,)*2))
-    def execute(self, message, user, params):
-        if params.group(6) is None:
-            target = Planet.load(*params.group(1,3,5))
-            if target is None:
-                message.alert("No planet with coords %s:%s:%s" % params.group(1,3,5))
-                return
-            attacker = self.get_user_planet(user)
-        else:
-            target = Planet.load(*params.group(1,3,5))
-            if target is None:
-                message.alert("No planet with coords %s:%s:%s" % params.group(1,3,5))
-                return
-            attacker = Planet.load(*params.group(6,8,10))
-            if attacker is None:
-                message.alert("No planet with coords %s:%s:%s" % params.group(6,8,10))
-                return
+    def planet(self, message, user, params):
+        target = Planet.load(*params.group(1,3,5))
+        if target is None:
+            message.alert("No planet with coords %s:%s:%s" % params.group(1,3,5))
+            return
+        attacker = Planet.load(*params.group(6,8,10))
+        if attacker is None:
+            message.alert("No planet with coords %s:%s:%s" % params.group(6,8,10))
+            return
 
         self.execute(message, target, attacker)
 
+    @route(loadable.planet_coord)
+    @require_planet
+    def me(self, message, user, params):
+        target = Planet.load(*params.group(1,3,5))
+        if target is None:
+            message.alert("No planet with coords %s:%s:%s" % params.group(1,3,5))
+            return
+        attacker = user.planet
+        
+        self.execute(message, target, attacker)
+    
     def execute(self, message, target, attacker):
         reply="Target "
         target_val = target.value
