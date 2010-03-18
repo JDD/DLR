743efaf32b9b5bccdb8a0d3312d2091f6e326364
JDD <Duncan@emailaccount.com>
2010-03-17 23:08:01 -0500

    refactored !roidcost so govs aren't hardcoded

--- Hooks/calcs/roidcost.py
+++ Hooks/calcs/roidcost.py
@@ -1,5 +1,5 @@
 # This file is part of Merlin.
-# Merlin is the Copyright (C)2008, 2009, 2010 of Robin K. Hansen, Elliot Rosemarine, Andreas Jacobsen.
+# Merlin is the Copyright (C)2008,2009,2010 of Robin K. Hansen, Elliot Rosemarine, Andreas Jacobsen.
 
 # Individual portions may be copyright by individual contributors, and
 # are included in this collective work with permission of the copyright
@@ -18,14 +18,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-
+ 
 from Core.paconf import PA
 from Core.loadable import loadable, route
 
 class roidcost(loadable):
     """Calculate how long it will take to repay a value loss capping roids."""
     usage = " <roids> <value_cost> [mining_bonus]"
-
+    
     @route(r"(\d+)\s+(\d+(?:\.\d+)?[km]?)(?:\s+(\d+))?")
     def execute(self, message, user, params):
         
@@ -39,13 +39,14 @@ class roidcost(loadable):
 
         mining=mining * ((float(bonus)+100)/100)
 
-        repay=int((cost*100)/(roids*mining))
-
-        reply="Capping %s roids at %s value with %s%% bonus will repay in %s ticks (%s days)" % (roids,self.num2short(cost),bonus,repay,repay/24)
-
-        repaydemo = int((cost*100)/(roids*mining*(1/(1+PA.getfloat("demo","prodcost")))))
-        repaytotal = int((cost*100)/(roids*mining*(1/(1+PA.getfloat("total","prodcost")))))
-        reply+=" Democracy: %s ticks (%s days)" % (repaydemo,repay/24)
-        reply+=" Totalitarianism: %s ticks (%s days)" % (repaytotal,repay/24)
-
+        repay = (cost*100)/(roids*mining)
+        reply = "Capping %s roids at %s value with %s%% bonus will repay in %s ticks (%s days)" % (roids,self.num2short(cost),bonus,int(repay),int(repay/24))
+        
+        for gov in PA.options("govs"):
+            bonus = PA.getfloat(gov, "prodcost")
+            if bonus == 0:
+                continue
+            repay_b = repay/(1+bonus)
+            reply += " %s: %s ticks (%s days)" % (PA.get(gov, "name"), int(repay_b), int(repay_b/24))
+        
         message.reply(reply)
