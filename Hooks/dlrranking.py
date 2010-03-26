e16c4c430e53c3eb9f5ca259e8ea9d2dd05d4de4
JDD <Duncan@emailaccount.com>
2010-03-26 14:27:15 -0500

    added a command to link to the arthur site

--- Hooks/dlrranking.py
+++ Hooks/dlrranking.py
@@ -1,5 +1,5 @@
 # This file is part of Merlin.
-# Merlin is the Copyright (C)2008-2009 of Robin K. Hansen, Elliot Rosemarine, Andreas Jacobsen.
+# Merlin is the Copyright (C)2008, 2009, 2010 of Robin K. Hansen, Elliot Rosemarine, Andreas Jacobsen.
 
 # Individual portions may be copyright by individual contributors, and
 # are included in this collective work with permission of the copyright
@@ -19,31 +19,13 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-# List of package modules
-__all__ = ["system",
-           "chanusertracker",
-           "auth",
-           "commandlog",
-           "help",
-           "user",
-#           "propsandcookies",
-           "sms",
-           "lookup",
-           "details",
-           "intel",
-           "growth",
-#           "target",
-           "mydef",
-           "victim",
-           "calcs",
-           "scans",
-           "ships",
-           "quotes",
-           "bcalc",
-           "galstatus",
-           "game",
-           "forums",
-           "stats",
-           "betastats",
-           "dlrranking",
-           ]
+from Core.loadable import loadable, route
+
+class dlrrankings(loadable):
+
+    @route()
+    def execute(self, message, user, params):
+        
+        ranking = ["http://www.darklordsrising.co.nr/"]
+
+        message.reply("DLR ranks website: "+" | ".join(ranking))
