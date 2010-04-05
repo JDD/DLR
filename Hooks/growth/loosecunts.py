98ea1617331f5fb8f799b87c2603a656b8655310
JDD <Duncan@emailaccount.com>
2010-04-04 19:06:02 -0500

    /equeens/

--- Hooks/growth/loosecunts.py
+++ Hooks/growth/loosecunts.py
@@ -20,9 +20,8 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
 from sqlalchemy.sql import desc
-from Core.config import Config
 from Core.db import session
-from Core.maps import Alliance, User, epenis
+from Core.maps import User, epenis
 from Core.loadable import loadable, route
 
 class lowscorers(loadable):
@@ -30,15 +29,11 @@ class lowscorers(loadable):
     @route(access = "member")
     def execute(self, message, user, params):
         
-        alliance = Alliance.load(Config.get("Alliance","name"))
-        if alliance is None:
-            message.reply("No alliance matching '%s' found"%(Config.get("Alliance","name"),))
-            return
         Q = session.query(User, epenis)
         Q = Q.join(User.epenis)
         Q = Q.order_by(desc(epenis.rank))
         result = Q[:5]
-        
+
         if len(result) < 1:
             message.alert("There is no Growth available yet.")
             return
