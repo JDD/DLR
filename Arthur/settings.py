44a27a2d4e597c73780c4dcd5fbae9fb4b50dfdb
JDD <Duncan@emailaccount.com>
2010-04-07 21:47:01 -0500

    changed timezone

--- Arthur/settings.py
+++ Arthur/settings.py
@@ -5,7 +5,7 @@
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/London'
+TIME_ZONE = 'America/North_Dakota/New_Salem'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
