acdfb8c038fd36ceaef834cc66cb5276b498eef9
John Duncan <Duncan@emailaccount.com>
2010-12-20 17:11:21 -0600

    fixed typo

--- Hooks/calcs/xp.py
+++ Hooks/calcs/xp.py
@@ -19,7 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-from Cor.paconf import PA
+from Core.paconf import PA
 from Core.maps import Planet
 from Core.loadable import loadable, route, require_planet
 
